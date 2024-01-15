1b86f92f62b202d91bb5274989878480cfcaf087
faranasj <faranasj@gmail.com>
2024-01-15 18:29:36 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Enter first number: ");
+int firstNum = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Enter operator: ");
+string operators = Console.ReadLine()!;
+Console.WriteLine("Enter second number: ");
+int secondNum = int.Parse(Console.ReadLine()!);
+
+var result = operators.ToLower() switch
+{
+    "+" => firstNum + secondNum,
+    "plus" => firstNum + secondNum,
+    "-" => firstNum - secondNum,
+    "minus" => firstNum - secondNum,
+    "*" => firstNum * secondNum,
+    "times" => firstNum * secondNum,
+    "%" => firstNum % secondNum,
+};
+
+Console.WriteLine($"{firstNum} { operators} { secondNum} = { result}");
