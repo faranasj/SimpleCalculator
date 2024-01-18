a746c29bf3cbd58950afd5ed8783c62fcfc7cd97
Jubreel, Faranas Oriyomi <107746460+faranasj@users.noreply.github.com>
2024-01-18 10:54:00 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,10 +1,12 @@
-﻿Console.WriteLine("Enter first number: ");
+// This section is where I ask for input from the user.
+Console.WriteLine("Enter first number: ");
 int firstNum = int.Parse(Console.ReadLine()!);
 Console.WriteLine("Enter operator: ");
 string operators = Console.ReadLine()!;
 Console.WriteLine("Enter second number: ");
 int secondNum = int.Parse(Console.ReadLine()!);
 
+// Here the new switch case syntax is used on the operators variable to perform arithmetical operations.
 var result = operators.ToLower() switch
 {
     "+" => firstNum + secondNum,
@@ -16,4 +18,5 @@ var result = operators.ToLower() switch
     "%" => firstNum % secondNum,
 };
 
+// Here the result is displayed
 Console.WriteLine($"{firstNum} { operators} { secondNum} = { result}");
